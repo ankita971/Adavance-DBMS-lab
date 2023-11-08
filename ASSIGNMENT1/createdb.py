8e3ded7f755a6fdd90602ba4c121f8dfb14ed815
Ankita <68505696+ankita971@users.noreply.github.com>
2023-11-08 23:17:24 +0530

    Delete ASSIGNMENT1 directory

--- ASSIGNMENT1/createdb.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-conn=mysql.connector.connect(host='localhost',user='root',password='Simran12@')
-mycursor=conn.cursor()
-mycursor.execute("SHOW DATABASES")
-for i in mycursor:
-    print(i)
