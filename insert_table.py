999fa5f16621f05641af546880989ba223e7b675
Ankita <68505696+ankita971@users.noreply.github.com>
2023-11-18 23:11:45 +0530

    Delete insert_table.py

--- insert_table.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(host='localhost',password='12345',user='root')
-mycursor=conn.cursor()
-mycursor.execute("insert into testdb.student values('Hari',21,22,82.5);")
-mycursor.execute("insert into testdb.student values('Hari',21,22,82.5);")
-mycursor.execute("insert into testdb.student values('Hari',21,22,82.5);")
-mycursor.execute("insert into testdb.student values('Hari',21,22,82.5);")
-conn.commit()
-conn.close()
