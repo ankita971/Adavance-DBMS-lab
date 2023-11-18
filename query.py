30d3368185f247b5eadaa49ab0b3dbdbcbe631f0
Ankita <68505696+ankita971@users.noreply.github.com>
2023-11-18 23:12:05 +0530

    Delete query.py

--- query.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(host='localhost',password='12345',user='root')
-mycursor=conn.cursor()
-mycursor.execute("select * from testdb.student;")
-for i in mycursor.fetchall():
-    print(i)
-conn.close()    
