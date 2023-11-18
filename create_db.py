806cea4d89b3aaba5af960675e0381b054f8cfe0
Ankita <68505696+ankita971@users.noreply.github.com>
2023-11-18 23:11:13 +0530

    Delete create_db.py

--- create_db.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(host='localhost',password='12345',user='root')
-mycursor=conn.cursor()
-mycursor.execute("show databases")
-for i in mycursor:
-    print(i)
