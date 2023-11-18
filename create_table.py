0ddddaf1b26e908e0e8fb86dc967292080fdf43f
Ankita <68505696+ankita971@users.noreply.github.com>
2023-11-18 23:11:28 +0530

    Delete create_table.py

--- create_table.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(host='localhost',password='12345',user='root')
-mycursor=conn.cursor()
-mycursor.execute("create table testdb.student (Name varchar(100),Rollno int(6), Age int(2),Mark float);")
-conn.close()
